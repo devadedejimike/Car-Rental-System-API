3637b19d25669e96afb7fd7c961be3126b06989a
OutsydBaby <amichdot@gmail.com>
2026-05-12 12:22:11 +0100

    feat: add user login function

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import User from "../models/userModel";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs"
 
 
 export const Register = async (req: Request, res: Response) => {
@@ -29,6 +30,38 @@ export const Register = async (req: Request, res: Response) => {
             message: 'Error creating new user',
             error
         })
-        console.log(error)
+        // console.log(error)
+    }
+}
+
+export const Login = async (req: Request, res: Response) => {
+    try {
+        const jwt_secret = process.env.JWT_SECRET as string;
+        const {email, password} = req.body;
+        // Find User in DB
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({ message: "Invalid Credentials"});
+        }
+        // Verify Password
+        const verifyPassword = bcrypt.compare(password, user.password)
+        if(!verifyPassword){
+            return res.status(400).json({ message: "Incorrect Password"})
+        }
+        // Generate Token 
+        const token = jwt.sign({id: user._id}, jwt_secret, { expiresIn: "7d"})
+        res.status(200).json({
+            status: 'Success',
+            token,
+            user,
+            message: 'User Login Successful'
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'Fail',
+            message: 'Login Error',
+            error
+        })
+        // console.log("Error: ", error)
     }
 }
