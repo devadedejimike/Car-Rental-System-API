6afa5997f66a84e6ab81b46e98b78276ba82371e
OutsydBaby <amichdot@gmail.com>
2026-05-12 12:54:55 +0100

    feat: add adminRoutes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import authRoutes from './routes/authRoutes'
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import adminRoutes from "./routes/adminRoutes"
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes)
+app.use('/api/admin', adminRoutes)
 
 
 export default app;
