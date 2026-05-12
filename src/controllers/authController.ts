3d9b99f4d8a791e9e730698ce221dff15f50f601
OutsydBaby <amichdot@gmail.com>
2026-05-12 11:55:13 +0100

    feat: add register user function

---
+++ src/controllers/authController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import User from "../models/userModel";
+import jwt from "jsonwebtoken";
+
+
+export const Register = async (req: Request, res: Response) => {
+    try {
+        const jwt_secret = process.env.JWT_SECRET as string;
+        const {username, email, password} = req.body;
+        // Check if user already exist
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({ message: 'User already exist'})
+        }
+        // If User doesn't exist
+        const newUser = await User.create({username, email, password});
+        const token = jwt.sign({id: newUser._id}, jwt_secret, {expiresIn: "7d"});
+        res.status(201).json({
+            status: 'Success',
+            token,
+            data: {
+                user: newUser
+            },
+            message: 'New User Created'
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'Fail',
+            message: 'Error creating new user',
+            error
+        })
+        console.log(error)
+    }
+}
