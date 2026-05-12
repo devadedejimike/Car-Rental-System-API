7be520f8d1ea3bd13ea543635f7c8d7cdfbf039f
OutsydBaby <amichdot@gmail.com>
2026-05-12 11:56:59 +0100

    feat: add authRoutes, connectDB, dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from './routes/authRoutes'
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes)
 
 
 export default app;
