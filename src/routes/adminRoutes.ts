4091ec3084cedd2d0a64da5f731014aee26f7437
OutsydBaby <amichdot@gmail.com>
2026-05-12 15:25:17 +0100

    feat: add update car details route to adminRoute

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createCar, getCars } from "../controllers/carController";
+import { createCar, getCars, UpdateCar } from "../controllers/carController";
 
 const router = Router();
 
 router
     .post('/create', createCar)
     .get('/cars', getCars)
+    .patch('/cars/:id', UpdateCar)
 
 
 export default router
