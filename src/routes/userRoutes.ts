93108cb04184a2f65fda716b2d66b95dc3d6842f
OutsydBaby <amichdot@gmail.com>
2026-05-13 14:57:59 +0100

    feat: add get all user's bookings route to userRoutes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { getACar, getCars } from "../controllers/carController";
 import { protect } from "../middleware/protect";
-import { CreateBooking } from "../controllers/bookingController";
+import { CreateBooking, getUserBooking } from "../controllers/bookingController";
 
 const router = Router();
 
@@ -9,6 +9,7 @@ router
     .get('/cars',protect, getCars) //Fetch all cars
     .get('/cars/:id',protect, getACar)
     .post('/booking', protect, CreateBooking)
+    .get('/booking', protect, getUserBooking)
 
 
 export default router;
