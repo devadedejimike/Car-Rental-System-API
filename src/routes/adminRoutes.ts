e4d3b9768bf23f49183b9c92eec6653a14e99d1a
OutsydBaby <amichdot@gmail.com>
2026-05-13 12:10:27 +0100

    feat: add get all bookings route to adminRoutes

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { createCar, deleteCar, getCars, UpdateCar } from "../controllers/carController";
 import { protect } from "../middleware/protect";
 import { isAdmin } from "../middleware/adminMiddleware";
+import { getAllBooking } from "../controllers/bookingController";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router
     .get('/cars', getCars)
     .patch('/cars/:id',protect, isAdmin, UpdateCar)
     .delete('/cars/:id', protect, isAdmin, deleteCar)
+    .get('/booking', protect, isAdmin, getAllBooking)
 
 export default router
