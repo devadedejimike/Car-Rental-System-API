6ed1e7a21f4fda49f1b4dffc07fdf29f8cfd460f
OutsydBaby <amichdot@gmail.com>
2026-05-13 11:40:36 +0100

    feat: add protect and isAdmin to ensure only admin can access routes in adminRoutes

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { createCar, deleteCar, getCars, UpdateCar } from "../controllers/carController";
+import { protect } from "../middleware/protect";
+import { isAdmin } from "../middleware/adminMiddleware";
 
 const router = Router();
 
 router
-    .post('/create', createCar)
+    .post('/create', protect, isAdmin, createCar)
     .get('/cars', getCars)
-    .patch('/cars/:id', UpdateCar)
-    .delete('/cars/:id', deleteCar)
+    .patch('/cars/:id',protect, isAdmin, UpdateCar)
+    .delete('/cars/:id', protect, isAdmin, deleteCar)
 
 export default router
