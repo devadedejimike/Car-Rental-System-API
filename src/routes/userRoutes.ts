ff18d295820bef5ce2550856475a2ac3f059dfa8
OutsydBaby <amichdot@gmail.com>
2026-05-13 11:43:03 +0100

    feat: add protect to ensure only logged in user can access routes and createBooking to userRoute

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { getACar, getCars } from "../controllers/carController";
+import { protect } from "../middleware/protect";
+import { CreateBooking } from "../controllers/bookingController";
 
 const router = Router();
 
 router
-    .get('/cars', getCars) //Fetch all cars
-    .get('/cars/:id', getACar)
+    .get('/cars',protect, getCars) //Fetch all cars
+    .get('/cars/:id',protect, getACar)
+    .post('/booking', protect, CreateBooking)
 
 
 export default router;
