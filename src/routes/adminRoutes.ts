2ba2bdc905bb51f3cac481e182b88d9f31a2cacd
OutsydBaby <amichdot@gmail.com>
2026-05-15 16:10:24 +0100

    feat: add getDashboardStats to admin routes

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createCar, deleteCar, getCars, UpdateCar } from "../controllers/carController";
 import { protect } from "../middleware/protect";
 import { isAdmin } from "../middleware/adminMiddleware";
-import { ApproveBooking, CancelBooking, getAllBooking } from "../controllers/bookingController";
+import { ApproveBooking, CancelBooking, getAllBooking, getDashboardStats } from "../controllers/bookingController";
 
 const router = Router();
 
@@ -14,5 +14,6 @@ router
     .get('/booking', protect, isAdmin, getAllBooking)
     .patch('/booking/:id/approve', protect, isAdmin, ApproveBooking )
     .patch('/booking/:id/cancel', protect, isAdmin, CancelBooking)
+    .get("/dashboard-stats", protect, isAdmin, getDashboardStats );
 
 export default router
