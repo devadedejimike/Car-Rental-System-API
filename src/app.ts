130a30362e5ab875512a97768bd6822ae407916c
OutsydBaby <amichdot@gmail.com>
2026-05-17 08:18:00 +0100

    moved dotenv to the top

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import authRoutes from './routes/authRoutes'
-import dotenv from "dotenv";
 import connectDB from "./config/db";
 import adminRoutes from "./routes/adminRoutes"
 import userRoutes from "./routes/userRoutes"
 
-dotenv.config();
+
 connectDB();
 
 const app = express();
