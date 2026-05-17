d92a99b3e396035f396cbb9df6c0ddebc0cfc7f8
OutsydBaby <amichdot@gmail.com>
2026-05-17 08:14:01 +0100

    feat: improved authRoutes

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,19 +1,76 @@
 import { Router } from "express";
-import { createCar, deleteCar, getCars, UpdateCar } from "../controllers/carController";
+import { createCar, deleteCar, getCars, UpdateCar} from "../controllers/carController";
+import {
+  ApproveBooking,
+  CancelBooking,
+  getAllBooking,
+  getDashboardStats,
+} from "../controllers/bookingController";
+
 import { protect } from "../middleware/protect";
 import { isAdmin } from "../middleware/adminMiddleware";
-import { ApproveBooking, CancelBooking, getAllBooking, getDashboardStats } from "../controllers/bookingController";
+
+import upload from "../config/multer";
 
 const router = Router();
 
-router
-    .post('/create', protect, isAdmin, createCar)
-    .get('/cars', getCars)
-    .patch('/cars/:id',protect, isAdmin, UpdateCar)
-    .delete('/cars/:id', protect, isAdmin, deleteCar)
-    .get('/booking', protect, isAdmin, getAllBooking)
-    .patch('/booking/:id/approve', protect, isAdmin, ApproveBooking )
-    .patch('/booking/:id/cancel', protect, isAdmin, CancelBooking)
-    .get("/dashboard-stats", protect, isAdmin, getDashboardStats );
+// CARS
+router.post(
+  "/cars",
+  protect,
+  isAdmin,
+  upload.single("image"),
+  createCar
+);
 
-export default router
+router.get(
+  "/cars",
+  getCars
+);
+
+router.put(
+  "/cars/:id",
+  protect,
+  isAdmin,
+  upload.single("image"),
+  UpdateCar
+);
+
+router.delete(
+  "/cars/:id",
+  protect,
+  isAdmin,
+  deleteCar
+);
+
+// BOOKINGS
+router.get(
+  "/booking",
+  protect,
+  isAdmin,
+  getAllBooking
+);
+
+router.patch(
+  "/booking/:id/approve",
+  protect,
+  isAdmin,
+  ApproveBooking
+);
+
+router.patch(
+  "/booking/:id/cancel",
+  protect,
+  isAdmin,
+  CancelBooking
+);
+
+// DASHBOARD
+router.get(
+  "/dashboard-stats",
+  protect,
+  isAdmin,
+  getDashboardStats
+);
+
+export default router;
