91cb02dcc88da6120472865e4365877212dfcd73
OutsydBaby <amichdot@gmail.com>
2026-05-18 17:59:57 +0100

    feat: add confirm booking payment route to adminRoute

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,76 +1,21 @@
 import { Router } from "express";
-import { createCar, deleteCar, getCars, UpdateCar} from "../controllers/carController";
-import {
-  ApproveBooking,
-  CancelBooking,
-  getAllBooking,
-  getDashboardStats,
-} from "../controllers/bookingController";
-
+import { createCar, deleteCar, getCars, UpdateCar } from "../controllers/carController";
+import { ApproveBooking, CancelBooking, confirmPayment, getAllBooking, getDashboardStats } from "../controllers/bookingController";
 import { protect } from "../middleware/protect";
 import { isAdmin } from "../middleware/adminMiddleware";
 
 import upload from "../config/multer";
 
 const router = Router();
-
-// CARS
-router.post(
-  "/cars",
-  protect,
-  isAdmin,
-  upload.single("image"),
-  createCar
-);
-
-router.get(
-  "/cars",
-  getCars
-);
-
-router.put(
-  "/cars/:id",
-  protect,
-  isAdmin,
-  upload.single("image"),
-  UpdateCar
-);
-
-router.delete(
-  "/cars/:id",
-  protect,
-  isAdmin,
-  deleteCar
-);
-
-// BOOKINGS
-router.get(
-  "/booking",
-  protect,
-  isAdmin,
-  getAllBooking
-);
-
-router.patch(
-  "/booking/:id/approve",
-  protect,
-  isAdmin,
-  ApproveBooking
-);
-
-router.patch(
-  "/booking/:id/cancel",
-  protect,
-  isAdmin,
-  CancelBooking
-);
-
-// DASHBOARD
-router.get(
-  "/dashboard-stats",
-  protect,
-  isAdmin,
-  getDashboardStats
-);
+router
+    .post("/cars",protect, isAdmin, upload.single("image"), createCar)
+    .get("/cars", getCars)
+    .put("/cars/:id", protect, isAdmin, upload.single("image"), UpdateCar)
+    .delete("/cars/:id", protect, isAdmin, deleteCar)
+    .get("/booking", protect, isAdmin, getAllBooking)
+    .patch("/booking/:id/approve", protect,  isAdmin,  ApproveBooking)  
+    .patch("/booking/:id/cancel", protect, isAdmin, CancelBooking)
+    .patch("/booking/:id/pay", protect, isAdmin, confirmPayment)
+    .get("/dashboard-stats", protect, isAdmin, getDashboardStats)
 
 export default router;
