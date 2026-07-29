3308a0cf71a25458e3d0e26e59b479817aa540b6
OutsydBaby <amichdot@gmail.com>
2026-07-29 14:27:27 +0100

    feat: changed saving images to cloudinary to saving to upload folder in codebase

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,8 +1,42 @@
-// src/config/multer.ts
 import multer from "multer";
+import path from "path";
+import fs from "fs";
 
-const storage = multer.memoryStorage();
+const uploadDir = "uploads/cars";
 
-const upload = multer({ storage });
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
-export default upload;
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, uploadDir);
+  },
+
+  filename(req, file, cb) {
+    const unique =
+      Date.now() + "-" + Math.round(Math.random() * 1e9);
+
+    cb(
+      null,
+      unique + path.extname(file.originalname)
+    );
+  },
+});
+
+const fileFilter: multer.Options["fileFilter"] = ( req, file, cb ) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed."));
+  }
+};
+
+export default multer({
+  storage,
+  fileFilter,
+
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5MB
+  },
+});
